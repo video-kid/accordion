c6aeb8c59b56a47a09ef56d8b6e8620adc2cb5f9
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2024-03-07 23:08:01 +0100

    CSS - Add globals and theme

--- src/pages/Tasks/TasksPage.tsx
+++ src/pages/Tasks/TasksPage.tsx
@@ -4,29 +4,39 @@ import Card from '../../components/Card/Card';
 import Group from '../../components/Group/Group';
 import { api } from '../../api/api';
 import { removeSpaces } from '../../utils/utils';
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 
 const fetcher = (url: string) => api.get(url).then((res) => res.data);
 
 const TasksPage = () => {
   const { data, isLoading } = useSWR<Array<group>>('/', fetcher);
+  const theme = useTheme();
 
-  if (isLoading) return <div>loading...</div>;
-  if (!data) return <div>error</div>;
+  const Page = styled('div')`
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.font.primary};
+  `;
+
+  if (isLoading) return <Page>loading...</Page>;
+  if (!data) return <Page>error</Page>;
 
   return (
-    <Card
-      progress={15}
-      title='Lodgify Grouped Tasks'>
-      <>
-        {data.map(({ tasks, name }) => (
-          <Group
-            name={name}
-            tasks={tasks}
-            key={removeSpaces(name)}
-          />
-        ))}
-      </>
-    </Card>
+    <Page>
+      <Card
+        progress={15}
+        title='Lodgify Grouped Tasks'>
+        <>
+          {data.map(({ tasks, name }) => (
+            <Group
+              name={name}
+              tasks={tasks}
+              key={removeSpaces(name)}
+            />
+          ))}
+        </>
+      </Card>
+    </Page>
   );
 };
 
